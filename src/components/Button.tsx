8b9e175483551740ab78b206d10b8f2c1fb46310
unknown <isabelad-carvalho@hotmail.com>
2022-10-16 00:05:17 -0300

    Creating landing page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,26 @@
 import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  className,
+  asChild,
+  ...props
+}: ButtonProps) {
   const Comp = asChild ? Slot : "button";
   return (
     <Comp
       className={clsx(
-        "text-black focus:ring-2 ring-white text-sm font-semibold font-sans py-4 px-3 transition-colors bg-cyan-500 rounded w-full hover:bg-cyan-300"
+        "text-black focus:ring-2 ring-white text-sm font-semibold font-sans py-3 px-4 transition-colors bg-cyan-500 rounded w-full hover:bg-cyan-300",
+        className
       )}
+      {...props}
     >
       {children}
     </Comp>
